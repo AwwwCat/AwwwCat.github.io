aa14b64aa8bae6936eb70c3bf07b49235258bc29
AwwwCat <2358245329@qq.com>
2024-05-01 02:16:21 +0800

    提交更新

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 function showImage() {
-    for (var i = 1; i < 5; i++) {
+    for (var i = 1; i <= 6; i++) {
         const imgElement = document.getElementById("sex_image" + String(i));
         imgElement.src = "./images/jian.png";
     }
